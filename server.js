a4e786efa5ef8f99fd5a27dc49caaedc7c3a56d9
Jerry Padgett <sjpadgett@gmail.com>
2021-09-22 23:17:43 -0400

    Hard code service running port to 6660. Next to ccdaservice port 6661

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: '5000' }));
 
-const LISTEN_PORT = process.env.CQM_EXECUTION_SERVICE_PORT || 8081; // Port to listen on
+const LISTEN_PORT = 6660; // Port to listen on. Hardcoded elsewhere to 6660 as a safe port.
 const SERVER_HOST = '127.0.0.1'; // Listen on loopback interface so not exposed to outside world
 const REQUIRED_PARAMS = ['measure', 'valueSets', 'patients']; // Required params for calculation
 
